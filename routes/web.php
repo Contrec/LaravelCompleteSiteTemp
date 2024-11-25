cbbb2d183a14d72b90bbe877f8bf1025b62f03b4
Christophe CONTRERAS-MORILLAS <contrec2002@yahoo.fr>
2024-11-25 16:16:29 +0100

    Added all content from local machine

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('main');
+});
+
+Route::get('/', function () {
+    return view('main');
+});
+
+// Products management
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
+// Users management
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
